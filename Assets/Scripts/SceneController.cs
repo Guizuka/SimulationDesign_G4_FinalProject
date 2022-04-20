9f71b9d53bc038e0915de7ae99ff49176a96928f
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-04-20 02:32:48 -0400

    Scenes added + functionality for Scene Manager

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void LoadVictory()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+    public void LoadLose()
+    {
+        SceneManager.LoadScene("LostScene");
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("CoffeeShop");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
